2c07172f82f6900774a7cd78ee31987709bcffc2
Alexander <webdevel70@gmail.com>
2024-12-12 20:30:11 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -50,7 +50,10 @@ alert(pls[i].parentElement.offsetHeight)
     alert('дом готов' + pls[i].parentElement.offsetHeight)
 console.log(pls[i].parentElement)
   }
-
+  window.addEventListener('load', r)
+  function r() {
+    alert('load' + pls[i].parentElement.offsetHeight)
+  }
 
 }
 
