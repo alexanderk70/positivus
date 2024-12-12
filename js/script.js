2abf1209d3a09b536188d770a47ea8f16fe5fa46
Alexander <webdevel70@gmail.com>
2024-12-12 20:15:50 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ for (let i = 0; i < pls.length; i++) {
       item.style.height = titleElement.offsetHeight + 'px'
     }
   })
-console.log(pls[i].parentElement)
+console.dir(pls[i].parentElement)
 alert(pls[i].parentElement.offsetHeight)
     pls[i].parentElement.parentElement.style.height =
       pls[i].parentElement.offsetHeight + 'px'
